9cecfe5434c3deca0ff44d93782400f731c85194
Maxiub <110039731+Maxiub@users.noreply.github.com>
2024-05-27 21:49:18 +0900

    Integrated weather API

--- src/function.js
+++ src/function.js
@@ -1,9 +1,26 @@
+function displayedWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityDisplay = document.querySelector("#city-display");
+  cityDisplay.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "43afd3dac1909o23bffcac359tc059f7";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiURL).then(displayedWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityDisplay = document.querySelector("#city-display");
-  cityDisplay.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Tokyo");
